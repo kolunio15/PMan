c397548995806325486b6b3c5d91036814843fde
kolunio <10465218-kolunio@users.noreply.gitlab.com>
2024-07-05 13:23:58 +0200

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,5 @@
+﻿using System.Windows;
+namespace PMan;
+public partial class App : Application
+{
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+namespace PMan;
+public partial class MainWindow : Window
+{
+	public MainWindow()
+	{
+		InitializeComponent();
+	}
+}
